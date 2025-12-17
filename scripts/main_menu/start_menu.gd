5da7645ef84c9f936b1d907acaafe5a6992f8fc7
Soufiane Nacer <nacer.soufiane31@example.com>
2025-12-17 11:43:45 +0100

    function menu

---
+++ scripts/main_menu/start_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
+
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
